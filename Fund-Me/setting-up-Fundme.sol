ddff3e2a431871ea1c551f1434d199d328430531
Wambugu Gichuki <wwambugu62@gmail.com>
2024-07-01 18:23:33 +0300

    creating the fund me contract which is going to be used to crowdsource for funds

---
+++ Fund-Me/setting-up-Fundme.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// a good habit before starting to write code is to write the list of steps you need to do
+
+// we need to get funds from users
+// withdraw funds
+// set a minimum funding value in USD
+
+contract FundMe {
+    // a function to send money to our contract
+    function fund() public {}
+
+    // a function that the owner of the contract is going to use to withdraw money
+    function withdraw() public {}
+}
+
+
